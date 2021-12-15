3de85a271b75caba96336d973c80aa3d49942bb9
Mukesh prajapati <94641465+Mukeshkingstar@users.noreply.github.com>
2021-12-15 14:21:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+function o(){
+  document.getElementById("main").style. display ="none"
+  document. getElementById("second"). style. display = "block"
+}function s(){
+  document.getElementById("main").style. display ="block"
+  document. getElementById("second"). style. display = "none"
+}
